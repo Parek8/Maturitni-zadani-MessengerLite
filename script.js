f9e933df825a7c6e1e50d06b58fadccde1f860b6
AdamMakoun <adammakaeu@gmail.com>
2023-03-31 18:05:46 +0200

    Some Validation Updates

--- script.js
+++ script.js
@@ -1,11 +1,48 @@
 function Validate(event)
 {
-    
+    let validFname, validLname, validUsername, validPassword, ValidConfPassword, validQuestion, validAnswer = true ;
 
     let fname = $("[name='first_name']").val();
-    let lname = $("[name='first_name']").val();
+    let lname = $("[name='last_name']").val();
+    let username = $("[name='username']").val();
+    //let password = $("[name='last_name']").val(); Dont know why this doesnt work on validation
+    let password = document.getElementById("myPsw").value;
+    let confirm_password = $("[name='confirm_password']").val();
+    let email = $("[name='email']").val(); /*I have no idea if we will use it for validation, because of htmls automatic validation for email */
+    let question = $("[name='question']").val();
+    let answer = $("[name='answer']").val();
 
-    return (fname == "");
+    if(fname == null || fname.replace(/^\s+|\s+$/gm,'') == ""){
+            alert("You need to input First Name");
+            validFname= false;    
+        }
+    if(lname == null || lname.replace(/^\s+|\s+$/gm,'') == ""){
+            alert("You need to input Last Name");
+            validLname = false;
+        }
+    if(username.length < 8 || username.replace(/^\s+|\s+$/gm,'') == ""){
+            alert("Your Username needs to be atleast 8 characters long");
+            validUsername = false;
+        }
+
+    if(password.length < 8){
+            alert("Your Password needs to be atleast 8 characters long");
+            validPassword = false;
+        }
+    if(confirm_password != password){
+            alert("Your Password needs to match with Confirm Password");
+            ValidConfPassword = false;
+        }    
+    if(question.replace(/^\s+|\s+$/gm,'') == ""){
+            alert("You need to input a question for account recovery");
+            validQuestion = false;
+        }
+    if(answer.replace(/^\s+|\s+$/gm,'') == ""){
+            alert("You need to input an answer for account recovery");
+            validAnswer = false
+        }
+    
+    return (validFname && validLname && validUsername && validPassword && ValidConfPassword && validQuestion && validAnswer);
 }
 
 function MistakesFound()
