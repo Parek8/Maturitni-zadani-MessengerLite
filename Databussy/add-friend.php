8c70da8f28f5c5a1aa1703d09c5ca14b8790f728
AdamMakoun <adammakaeu@gmail.com>
2023-04-28 09:57:12 +0200

    fixed removing friend request notifications from database. User Parecek8 is somehow the only one who sends those notifications twice probably way too thich user.

--- Databussy/add-friend.php
+++ Databussy/add-friend.php
@@ -5,13 +5,17 @@ $type = $_POST['type'];
 global $myId;
 if($type == "sendFriendRequest")
 {
+    $inserted = true;
     global $connect;
     global $myId;
-
-    $insertQuery = "INSERT INTO friend_requests(sender_id, reciever_id) VALUES(".$myId['id'].", ".$_POST['newFriendId'].");";
-    $notifQuery = " INSERT INTO notifications VALUES(null, 'Friend Request', 'Someone has sent you a friend request!',".$myId['id'].", ".$_POST['newFriendId'].")";
-    $connect->query($insertQuery);
-    $connect->query($notifQuery);
+    if($inserted){
+        $insertQuery = "INSERT INTO friend_requests(sender_id, reciever_id) VALUES(".$myId['id'].", ".$_POST['newFriendId'].");";
+        $notifQuery = " INSERT INTO notifications VALUES(null, 'Friend Request', 'Someone has sent you a friend request!',".$myId['id'].", ".$_POST['newFriendId'].")";
+        $connect->query($insertQuery);
+        $connect->query($notifQuery);
+        $inserted = false;
+    }   
+    
 }
 else if($type == "acceptFriendRequest")
 {
@@ -22,12 +26,13 @@ else if($type == "acceptFriendRequest")
     $results = $connect->query($findRequests);
     while($result = $results->fetch_assoc())
     {
+        $friendId = intval($_POST["newFriendId"]);
+        $mineId = intval($_POST["myId"]);
         $addFriendQuery = "INSERT INTO friends(sender_id, reciever_id) VALUES(".$_POST["newFriendId"].",".$_POST["myId"].");";
-        $removeFriendRequestNotificationQueryFromDeliciousDataBussy = 'DELETE FROM notifications WHERE notifications.sender_id ='. $_POST["newFriendId"] . ' AND notifications.reciever_id = '. $myId['id'] . 'AND notifications.type = "Friend Request" OR notifications.sender_id ='.  $myId['id'] . ' AND notifications.reciever_id = '.$_POST["newFriendId"] . 'AND notifications.type = "Friend Request" ';
+        $removeFriendRequestNotificationQueryFromDeliciousDataBussy = 'DELETE FROM notifications WHERE (notifications.sender_id = '.$mineId.' AND notifications.reciever_id = '.$friendId.' AND notifications.type = "Friend Request") OR (notifications.sender_id = '.$friendId.' AND notifications.reciever_id = '.$mineId.' AND notifications.type = "Friend Request")';
         $deleteQuery = "DELETE FROM friend_requests WHERE id = ".$result['id']."";
         $connect->query($addFriendQuery);
         $connect->query($deleteQuery);
-        // POŘEŠIT ID, PROTOŽE NĚJAK NEFACHAJÍ...QUERY JE OK! FURT TO NEJEDE NEVIM
         $connect->query($removeFriendRequestNotificationQueryFromDeliciousDataBussy);
     }
 }
